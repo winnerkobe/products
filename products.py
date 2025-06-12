3dcd555c796d4e62431a837d31303061ea9112bb
winnerkobe <hsiehziping@gmail.com>
2025-06-12 12:08:15 +0800

    Add files via upload

---
+++ products.py
@@ -0,0 +1,10 @@
+print('輸入q終止程式。')
+products = []
+while True:
+	name = input('請輸入商品名稱。')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格。')
+	products.append([name, price])
+print(f'你輸入的商品有{products}。')
+print(f'{products[0][0]}是{products[0][1]}元')
